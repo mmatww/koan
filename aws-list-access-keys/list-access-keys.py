2c712db9bb6c252071eda4861bd05479b0cb4b1f
Marti Martinez <89093153+mmatww@users.noreply.github.com>
2023-01-11 11:55:35 -0700

    Create list-access-keys.py

---
+++ aws-list-access-keys/list-access-keys.py
@@ -0,0 +1,22 @@
+import boto3
+import os
+
+AWS_PROFILE = os.getenv("AWS_PROFILE", "default")
+
+boto3.setup_default_session(profile_name=AWS_PROFILE)
+resource = boto3.resource("iam")
+client = boto3.client("iam")
+
+for user in resource.users.all():
+    Metadata = client.list_access_keys(UserName=user.user_name)
+    if Metadata["AccessKeyMetadata"]:
+        for key in user.access_keys.all():
+            AccessId = key.access_key_id
+            Status = key.status
+            LastUsed = client.get_access_key_last_used(AccessKeyId=AccessId)
+            status = "INACTIVE"
+            if (Status == "Active"):
+                status = "NEVER USED"
+                if "LastUsedDate" in LastUsed["AccessKeyLastUsed"]:
+                    status = str(LastUsed["AccessKeyLastUsed"]["LastUsedDate"])[:10]
+            print(f"Key: {AccessId}\tCreated: {str(key.create_date)[:10]}\tStatus: {status}\tUser: {user.user_name}")
